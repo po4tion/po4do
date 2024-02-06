416ea7f0eec9413f0e69e684b6ec33dccabd2a92
po4tion <po4tion0429@gmail.com>
2024-02-06 22:58:35 +0900

    build(#13): add react-query

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   plugins: ['react-refresh', '@stylexjs'],
+  extends: ['plugin:@tanstack/eslint-plugin-query/recommended'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './constants/router.tsx';
 import './index.css';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      staleTime: 60000,
+      placeholderData: true,
+    },
+  },
+});
+
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
