ceb289df54bd68d3f4ab1db7a56e5752932f0c2d
po4tion <po4tion0429@gmail.com>
2024-02-15 01:43:47 +0900

    feat(#15): styling and setting event

--- src/pages/service/components/Todo/List.tsx
+++ src/pages/service/components/Todo/List.tsx
@@ -10,12 +10,24 @@ const listStyles = create({
   },
 
   listitem: {
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+
     padding: '1rem 1.5rem',
 
     marginBottom: '0.3rem',
 
     border: '1px solid hsl(0deg 0% 80%)',
     borderRadius: '0.2rem',
+
+    fontSize: '1rem',
+
+    cursor: 'pointer',
+
+    ':hover': {
+      background: 'hsl(0deg 0% 90%)',
+    },
 
     ':first-of-type': {
       borderTopRightRadius: '0.5rem',
@@ -29,20 +41,47 @@ const listStyles = create({
       borderBottomLeftRadius: '0.5rem',
     },
   },
+
+  done: {
+    textDecoration: 'line-through',
+    textDecorationColor: 'hsl(0deg 100% 70%)',
+    color: 'hsl(0deg 0% 60%)',
+  },
 });
 
 type Props = {
   data: Array<Database['public']['Tables']['todos']['Row']>;
+  update: (
+    id: Database['public']['Tables']['todos']['Update']['id'],
+    status: Database['public']['Tables']['todos']['Update']['status'],
+  ) => void;
+  remove: (id: Database['public']['Tables']['todos']['Update']['id']) => void;
 };
 
-export const List = ({ data }: Props) => {
+export const List = ({ data, update, remove }: Props) => {
   return (
     <ul {...props(listStyles.list)}>
-      {data.map(({ id, description }) => (
-        <li key={id} {...props(listStyles.listitem)}>
-          {description}
-        </li>
-      ))}
+      {data.map(({ id, description, status }) => {
+        const styles = [
+          listStyles.listitem,
+          status === 'done' ? listStyles.done : {},
+        ];
+
+        return (
+          <li
+            key={id}
+            onClick={() => {
+              update(id, status);
+            }}
+            onDoubleClick={() => {
+              remove(id);
+            }}
+            {...props(...styles)}
+          >
+            {description}
+          </li>
+        );
+      })}
     </ul>
   );
 };
