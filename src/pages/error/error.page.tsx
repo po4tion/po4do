3e18a7faa64c4f1ffa86fe05aa1117bf2fef9a52
po4tion <po4tion0429@gmail.com>
2024-02-22 02:57:01 +0900

    feat(#27): Develop buttons in error page

--- src/pages/error/error.page.tsx
+++ src/pages/error/error.page.tsx
@@ -1,17 +1,42 @@
-import { create } from '@stylexjs/stylex';
+import { Button, Link } from '@/components/Button';
+import { create, props } from '@stylexjs/stylex';
+import { useNavigate } from 'react-router-dom';
 import { NotFound } from './components/NotFound';
 
 const errorStyles = create({
-  notFound: {
+  container: {
     width: '40rem',
+  },
+  notFound: {
+    width: '100%',
     aspectRatio: 1 / 1,
+  },
+  buttonGroup: {
+    width: '100%',
+
+    display: 'flex',
+    columnGap: '2rem',
+  },
+  link: {
+    background: 'hsl(344deg 90.8% 74.3%)',
   },
 });
 
 export const ErrorPage = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <div {...props(errorStyles.container)}>
       <NotFound style={errorStyles.notFound} />
+      <div {...props(errorStyles.buttonGroup)}>
+        <Button type="button" onClick={() => navigate(-1)}>
+          Back
+        </Button>
+
+        <Link to="/" replace style={errorStyles.link}>
+          Home
+        </Link>
+      </div>
     </div>
   );
 };
