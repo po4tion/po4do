9c23b304f999efe4c93a9684720a8375ccb8f24d
po4tion <po4tion0429@gmail.com>
2024-02-22 01:03:08 +0900

    feat(#26): add notFound rive

--- src/pages/error/error.page.tsx
+++ src/pages/error/error.page.tsx
@@ -1,3 +1,17 @@
+import { create } from '@stylexjs/stylex';
+import { NotFound } from './components/NotFound';
+
+const errorStyles = create({
+  notFound: {
+    width: '40rem',
+    aspectRatio: 1 / 1,
+  },
+});
+
 export const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div>
+      <NotFound style={errorStyles.notFound} />
+    </div>
+  );
 };
