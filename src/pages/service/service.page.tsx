e999b9ffee027baf11e213aaa4b1c8dbdd80b6c7
po4tion <po4tion0429@gmail.com>
2024-03-07 00:14:08 +0900

    feat(#28): hide server logic to updateTodo hook

--- src/pages/service/service.page.tsx
+++ src/pages/service/service.page.tsx
@@ -4,8 +4,7 @@ import { useRemoveTodo } from '@/server/todos/mutations/useRemoveTodo';
 import { useUpdateTodo } from '@/server/todos/mutations/useUpdateTodo';
 import { useGetTodos } from '@/server/todos/queries';
 import { TODOS_KEYS } from '@/server/todos/queries/keys';
-import { useQueryClient } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useState, type ComponentProps } from 'react';
 import { Container } from './components/Container';
 import { Title } from './components/Title';
 import { Todo } from './components/Todo';
@@ -13,10 +12,9 @@ import { Write } from './components/Write';
 
 export const ServicePage = withSuspense(() => {
   const { data: todos } = useGetTodos();
-  const queryClient = useQueryClient();
-  const { mutate: createTodoMutate } = useCreateTodo();
   const [todo, setTodo] = useState('');
 
+  const { mutate: createTodoMutate } = useCreateTodo();
   const createTodo = () => {
     createTodoMutate({
       description: todo,
@@ -25,35 +23,17 @@ export const ServicePage = withSuspense(() => {
   };
 
   const { mutate: updateTodoMutate } = useUpdateTodo();
-
-  const updateTodo = (
-    id: Database.TablesUpdate<'todos'>['id'],
-    status: Database.TablesUpdate<'todos'>['status'],
+  const updateTodo: ComponentProps<typeof Todo.List>['update'] = (
+    id,
+    status,
   ) => {
-    if (!id) {
-      alert('id 값이 유효하지 않습니다.');
-      return;
-    }
-
-    const updateStatusTodo: Pick<
-      Database.TablesUpdate<'todos'>,
-      'id' | 'status'
-    > = {
+    updateTodoMutate({
       id,
       status: status === 'progress' ? 'done' : 'progress',
-    };
-
-    updateTodoMutate(updateStatusTodo, {
-      onSuccess: () => {
-        queryClient.invalidateQueries({
-          queryKey: TODOS_KEYS.todos(getTodoParameters),
-        });
-      },
     });
   };
 
   const { mutate: removeTodoMutate } = useRemoveTodo();
-
   const removeTodo = (id: Database.TablesUpdate<'todos'>['id']) => {
     removeTodoMutate(id, {
       onSuccess: () => {
